ea7a36b9bcfebd1fbe317dec92f05a8d884de763
kobster68 <kobe.laprade@gmail.com>
2023-10-29 15:25:09 -0400

    added comments to code
    added some comments to the code. not yet complete with adding comments.

--- code-soup/main.cpp
+++ code-soup/main.cpp
@@ -6,15 +6,18 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+// settings for window size
 const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 600;
 
+// vertex shader (c code to be compiled during runtime)
 const char* vertexShaderSource = "#version 460 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
+// fragment shader (c code to be compiled during runtime)
 const char* fragmentShaderSource = "#version 460 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
@@ -22,47 +25,67 @@ const char* fragmentShaderSource = "#version 460 core\n"
 "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
 "}\n\0";
 
+// the main function that runs when the program starts
 int main() {
 
+    // initializes glfw
     glfwInit();
+    // sets the glfw version to 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // sets glfw profile to "core"
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
+    // sets glfw profile to "compatability" if on apple os
     #ifdef __APPLE__
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     #endif
 
+    // creates the glfw window with the width, height and title
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "soup", NULL, NULL);
+    // if window fails to create;
     if (window == NULL)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        // send an error message
+        std::cout << "failed to create glfw window" << std::endl;
+        // end the program and glfw
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
+    // sets up a callback to the "framebuffer_size_callback" function
+    // when the window is resized it calls the function
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // loads glad and checks if it loads
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        // if it fails send message and end program
+        std::cout << "failed to initialize glad" << std::endl;
         return -1;
     }
 
+    // 
     glViewport(0, 0, WIDTH, HEIGHT);
 
+    // creates a vertex shader
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // uses the previously defined vertex shader c code
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    // and compiles it
     glCompileShader(vertexShader);
 
-    int  success;
+    // creates an int to store shader state
+    int success;
+    // char array to store error information
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
     if (!success)
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "vertex shader failed to compile\n" << infoLog << std::endl;
     }
 
     unsigned int fragmentShader;
@@ -74,7 +97,7 @@ int main() {
     if (!success)
     {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "fragment shader failed to compile\n" << infoLog << std::endl;
     }
 
     unsigned int shaderProgram;
@@ -87,8 +110,9 @@ int main() {
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        std::cout << "shader program failed to link\n" << infoLog << std::endl;
     }
+    // deletes the shaders after linking them to opengl
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
@@ -119,12 +143,15 @@ int main() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // the main rendering loop
+    // as long as glfw isnt pending to close
+    // the loop will run
     while (!glfwWindowShouldClose(window))
     {
-        // input
+        // input section
         processInput(window);
 
-        // rendering
+        // rendering section
         glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -132,22 +159,29 @@ int main() {
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // events and buffers
+        // events and buffers section
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
+    
+    // if the rendering loop ends
+    // glfw is terminated and the program ends
     glfwTerminate();
     return 0;
 }
 
+// this function processes user input
 void processInput(GLFWwindow* window)
 {
+    // if the user presses escape
+    // tell glfw to close the window
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
+// this function is called when the window is resized
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+    // updates the viewport size to the new window size
     glViewport(0, 0, width, height);
 }
